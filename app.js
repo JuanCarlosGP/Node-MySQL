b0cde40f51048a9a543a9c81cf3c5aab6bb4dbee
JuanCarlosGP <64562942+JuanCarlosGP@users.noreply.github.com>
2024-06-02 02:07:28 +0200

    asd

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
-
-import mysql from 'mysql2'
-
-import dotenv from 'dotenv'
-dotenv.config()
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE
-}).promise()
-const result = await pool.query('SELECT * FROM etiquetas')
+}).promise();
 
-const rows = result[0]
-console.log(result)
+const insertQuery = `INSERT INTO usuarios (Nombre_Usuario, Correo_Electrónico, Contraseña) VALUES (?, ?, ?)`;
+const values = ['Juan Perez', 'juan.perez@example.com', 'contraseña123'];
 
+try {
+  const result = await pool.execute(insertQuery, values);
+  console.log('Usuario insertado:', result);
+} catch (error) {
+  console.error('Error al insertar usuario:', error);
+}
 
 // import mysql from 'mysql2'
 
